b6916bbf60c7299b36ef0e86a52c48959e7ac600
wr777718 <123456789@qq.com>
2019-12-11 17:29:06 +0800

    redis 字符串实现 密码错误3次

--- 19-12-11/login.php
+++ 19-12-11/login.php
@@ -5,3 +5,24 @@
  * Date: 2019/12/11
  * Time: 17:12
  */
+//判断登录次数   3次以上 过一个小时后在登录
+$redis = new Redis();
+$redis->connect('localhost');
+//$redis->auth();
+//初始化一个  用户的唯一昵称
+$userName = $_POST['username'].'_username';
+$passWord = '123456';
+$number = $redis->get($userName);
+if( $number > 3 ){
+    echo '登录上限，请明天在登录';
+    die;
+}
+if( $_POST['password'] == $passWord ){
+    echo '登录成功';
+} else{
+    $totalNum = 3;
+    $nowNum = $totalNum - $number;
+    $redis->incr($userName);
+    $redis->setTimeout($userName,3600);
+    echo '密码错误，您还有'.$nowNum.'次机会';
+}
